796682353da4d59342460ed8a9faac050c6e64f1
Quinnlizzy <quin27@hotmail.co.uk>
2024-01-19 11:53:19 +0000

    lesson11

---
+++ lesson11.py
@@ -0,0 +1,9 @@
+name='TERRY'
+color = 'RED'
+msg = '[' + name + '] loves the color ' + color.lower() + '!'
+msg1 = f'[{name.capitalize()}] loves the color {color.lower()}!'
+print(msg)
+print(msg1)
+
+# ›[TERRY] loves the color red!
+# ›[Terry] loves the color red!
