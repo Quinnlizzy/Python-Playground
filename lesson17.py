c008ad2f9ceefca66a7948772da4b1308d2480a8
Quinnlizzy <quin27@hotmail.co.uk>
2024-01-24 09:03:18 +0000

    split & join

--- lesson17.py
+++ lesson17.py
@@ -4,3 +4,55 @@ friends_list = ['Eric','John','Michael','Terry','Graham']
 print(list(msg))
 
 # ›['W', 'e', 'l', 'c', 'o', 'm', 'e', ' ', 't', 'o', ' ', 'P', 'y', 't', 'h', 'o', 'n', ' ', '1', '0', '1', ':', ' ', 'S', 'p', 'l', 'i', 't', ' ', 'a', 'n', 'd', ' ', 'J', 'o', 'i', 'n']
+
+
+
+
+
+msg ='Welcome to Python 101: Split and Join'
+csv = 'Eric,John,Michael,Terry,Graham'
+friends_list = ['Eric','John','Michael','Terry','Graham']
+print(msg.split())
+
+#['Welcome', 'to', 'Python', '101:', 'Split', 'and', 'Join']
+
+
+
+
+msg ='Welcome to Python 101: Split and Join'
+csv = 'Eric,John,Michael,Terry,Graham'
+friends_list = ['Eric','John','Michael','Terry','Graham']
+print(msg.split())
+print(msg.split(' '))
+
+#['Welcome', 'to', 'Python', '101:', 'Split', 'and', 'Join']
+#['Welcome', 'to', 'Python', '101:', 'Split', 'and', 'Join']
+
+
+
+
+
+msg ='Welcome  to  Python  101: Split  and Join'
+csv = 'Eric,John,Michael,Terry,Graham'
+friends_list = ['Eric','John','Michael','Terry','Graham']
+print(msg.split())
+print(msg.split(' '), type(msg.split(' ')))
+
+#['Welcome', 'to', 'Python', '101:', 'Split', 'and', 'Join']
+#['Welcome', '', 'to', '', 'Python', '', '101:', 'Split', '', 'and', 'Join'] <class 'list'>
+
+
+
+
+
+msg ='Welcome  to  Python  101: Split  and Join'
+csv = 'Eric,John,Michael,Terry,Graham'
+friends_list = ['Eric','John','Michael','Terry','Graham']
+print(csv.split(','))
+
+#['Eric', 'John', 'Michael', 'Terry', 'Graham']
+
+
+
+
+
