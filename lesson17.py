b89ccc50dfd717b0acc56cb3066501dfa1e5597b
Quinnlizzy <quin27@hotmail.co.uk>
2024-01-24 08:59:00 +0000

    list msg

---
+++ lesson17.py
@@ -0,0 +1,6 @@
+msg ='Welcome to Python 101: Split and Join'
+csv = 'Eric,John,Michael,Terry,Graham'
+friends_list = ['Eric','John','Michael','Terry','Graham']
+print(list(msg))
+
+# ›['W', 'e', 'l', 'c', 'o', 'm', 'e', ' ', 't', 'o', ' ', 'P', 'y', 't', 'h', 'o', 'n', ' ', '1', '0', '1', ':', ' ', 'S', 'p', 'l', 'i', 't', ' ', 'a', 'n', 'd', ' ', 'J', 'o', 'i', 'n']
