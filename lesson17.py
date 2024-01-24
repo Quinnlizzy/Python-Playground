d3149d770668a0871fc42954e819215d2dc3257b
Quinnlizzy <quin27@hotmail.co.uk>
2024-01-24 09:07:44 +0000

    split&join fin

--- lesson17.py
+++ lesson17.py
@@ -56,3 +56,44 @@ print(csv.split(','))
 
 
 
+msg ='Welcome  to  Python  101: Split  and Join'
+csv = 'Eric,John,Michael,Terry,Graham'
+friends_list = ['Eric','John','Michael','Terry','Graham']
+print('-'.join(friends_list))
+
+#Eric-John-Michael-Terry-Graham
+
+
+
+msg ='Welcome  to  Python  101: Split  and Join'
+csv = 'Eric,John,Michael,Terry,Graham'
+friends_list = ['Eric','John','Michael','Terry','Graham']
+print(''.join(friends_list))
+
+#EricJohnMichaelTerryGraham
+
+
+
+msg ='Welcome  to  Python  101: Split  and Join'
+csv = 'Eric,John,Michael,Terry,Graham'
+friends_list = ['Eric','John','Michael','Terry','Graham']
+print(''.join(friends_list))
+
+print(''.join(msg.split()))
+
+#EricJohnMichaelTerryGraham
+#WelcometoPython101:SplitandJoin
+
+
+
+msg ='Welcome  to  Python  101: Split  and Join'
+csv = 'Eric,John,Michael,Terry,Graham'
+friends_list = ['Eric','John','Michael','Terry','Graham']
+print(''.join(friends_list))
+
+print(''.join(msg.split()))
+print(msg.replace(' ', ''))
+
+#EricJohnMichaelTerryGraham
+#WelcometoPython101:SplitandJoin
+#WelcometoPython101:SplitandJoin
