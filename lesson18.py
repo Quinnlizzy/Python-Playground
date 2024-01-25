e55b058bac0531cb8f51005c771da5c05a7527d8
Quinnlizzy <quin27@hotmail.co.uk>
2024-01-25 21:32:40 +0000

    18second

--- lesson18.py
+++ lesson18.py
@@ -1,7 +1,52 @@
-msg ='Welcome to Python 101: Split and Join'
-csv = 'Eric,John,Michael,Terry,Graham'
-friends_list = ['Eric','John','Michael','Terry','Graham']
-print(msg.split())
+
+csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
+friends_list = ['Exercise: fill me with names']
+print(friends_list)
+# From the list above fill a list(friends_list) properly
+# with the names of all the friends. One per "slot"
+# you may need to run same command several times
+# use print() statements to work your way through the exercise
+
+#['Exercise: fill me with names']
+
+csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
+print(','.join(csv.split(';')))
+print(','.join(csv.split(';')).split(':'))
+friends_list = ['Exercise: fill me with names']
+print(friends_list)
+# From the list above fill a list(friends_list) properly
+# with the names of all the friends. One per "slot"
+# you may need to run same command several times
+# use print() statements to work your way through the exercise
+
+#›Eric,John,Michael,Terry,Graham:TerryG,Brian
+#›['Eric,John,Michael,Terry,Graham', 'TerryG,Brian']
+#›['Exercise: fill me with names']
 
 
-#['Welcome', 'to', 'Python', '101:', 'Split', 'and', 'Join']
+
+csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
+print(','.join(csv.split(';')))
+print(','.join(csv.split(';')).split(':'))
+print(','.join(','.join(csv.split(';')).split(':')))
+friends_list = ['Exercise: fill me with names']
+print(friends_list)
+
+# ›Eric,John,Michael,Terry,Graham:TerryG,Brian
+# ›['Eric,John,Michael,Terry,Graham', 'TerryG,Brian']
+# ›Eric,John,Michael,Terry,Graham,TerryG,Brian
+# ›['Exercise: fill me with names']
+
+
+
+csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
+print((','.join(','.join(csv.split(';')).split(':'))).split(','))
+friends_list = ['Exercise: fill me with names']
+print(friends_list)
+# From the list above fill a list(friends_list) properly
+# with the names of all the friends. One per "slot"
+# you may need to run same command several times
+# use print() statements to work your way through the exercise
+
+# ›['Eric', 'John', 'Michael', 'Terry', 'Graham', 'TerryG', 'Brian']
+# ›['Exercise: fill me with names']
