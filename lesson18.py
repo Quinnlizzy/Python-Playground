aa79d684c20ec23845884d8acac2c41b4c512df9
Quinnlizzy <quin27@hotmail.co.uk>
2024-01-25 21:26:13 +0000

    18start

---
+++ lesson18.py
@@ -0,0 +1,7 @@
+msg ='Welcome to Python 101: Split and Join'
+csv = 'Eric,John,Michael,Terry,Graham'
+friends_list = ['Eric','John','Michael','Terry','Graham']
+print(msg.split())
+
+
+#['Welcome', 'to', 'Python', '101:', 'Split', 'and', 'Join']
