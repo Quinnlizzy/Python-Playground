4ac286eae30725e72c39040ca7511cb25c3d66a8
Quinnlizzy <quin27@hotmail.co.uk>
2024-01-25 21:39:24 +0000

    tuples

---
+++ lesson19.py
@@ -0,0 +1,8 @@
+#Tuples - faster Lists you can't change
+friends = ['John','Michael','Terry','Eric','Graham']
+friends_tuple = ('John','Michael','Terry','Eric','Graham')
+print(friends)
+print(friends_tuple)
+
+# ›['John', 'Michael', 'Terry', 'Eric', 'Graham']
+# ›('John', 'Michael', 'Terry', 'Eric', 'Graham')
