c7ad9df843c676c6277a01fede52856ae092c065
Quinnlizzy <quin27@hotmail.co.uk>
2024-01-25 21:41:25 +0000

    tuples slice

--- lesson19.py
+++ lesson19.py
@@ -17,3 +17,16 @@ print(friends_tuple[2])
 
 # ›Terry
 # ›Terry
+
+
+
+#Tuples - faster Lists you can't change
+friends = ['John','Michael','Terry','Eric','Graham']
+friends_tuple = ('John','Michael','Terry','Eric','Graham')
+print(friends[2:4])
+print(friends_tuple[2:4])
+
+
+
+# ›['Terry', 'Eric']
+# ›('Terry', 'Eric')
