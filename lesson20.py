c3ed272098a016904d4c8e7063318ce8f685b4f4
Quinnlizzy <quin27@hotmail.co.uk>
2024-01-26 15:14:02 +0000

    sets union

--- lesson20.py
+++ lesson20.py
@@ -37,3 +37,16 @@ my_friends_set = {'Reg','Loretta','Colin','Eric','Graham'}
 print(friends_set.difference(my_friends_set))
 
 #{'John', 'Michael', 'Terry'}
+
+
+
+
+#Sets - blazingly fast unordered Lists 
+friends = ['John','Michael','Terry','Eric','Graham']
+friends_tuple = ('John','Michael','Terry','Eric','Graham')
+friends_set = {'John','Michael','Terry','Eric','Graham','Eric'}
+my_friends_set = {'Reg','Loretta','Colin','Eric','Graham'}
+
+print(friends_set.union(my_friends_set))
+
+#{'John', 'Michael', 'Terry', 'Eric', 'Graham', 'Reg', 'Loretta', 'Colin'}
