91bf589ce9371c0e777341f5f436ba401c507f97
Quinnlizzy <quin27@hotmail.co.uk>
2024-01-26 15:12:45 +0000

    sets diff

--- lesson20.py
+++ lesson20.py
@@ -25,3 +25,15 @@ print(friends_set.intersection(my_friends_set))
 #{'Eric', 'Graham'}
 
 
+
+
+
+#Sets - blazingly fast unordered Lists 
+friends = ['John','Michael','Terry','Eric','Graham']
+friends_tuple = ('John','Michael','Terry','Eric','Graham')
+friends_set = {'John','Michael','Terry','Eric','Graham','Eric'}
+my_friends_set = {'Reg','Loretta','Colin','Eric','Graham'}
+
+print(friends_set.difference(my_friends_set))
+
+#{'John', 'Michael', 'Terry'}
