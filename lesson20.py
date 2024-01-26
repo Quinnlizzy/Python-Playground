3d04d19388f1598b1370502d75f8ef2d02ac114a
Quinnlizzy <quin27@hotmail.co.uk>
2024-01-26 15:09:54 +0000

    sets

---
+++ lesson20.py
@@ -0,0 +1,12 @@
+#Sets - blazingly fast unordered Lists 
+friends = ['John','Michael','Terry','Eric','Graham']
+friends_tuple = ('John','Michael','Terry','Eric','Graham')
+friends_set = {'John','Michael','Terry','Eric','Graham','Eric'}
+print(friends)
+print(friends_tuple)
+print(friends_set)
+
+
+# ›['John', 'Michael', 'Terry', 'Eric', 'Graham']
+# ›('John', 'Michael', 'Terry', 'Eric', 'Graham')
+# ›{'John', 'Michael', 'Terry', 'Eric', 'Graham'}
