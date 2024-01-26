337254b3a2bc296e34d8369b25c45bc5d3759424
Quinnlizzy <quin27@hotmail.co.uk>
2024-01-26 15:11:52 +0000

    sets2

--- lesson20.py
+++ lesson20.py
@@ -10,3 +10,18 @@ print(friends_set)
 # ›['John', 'Michael', 'Terry', 'Eric', 'Graham']
 # ›('John', 'Michael', 'Terry', 'Eric', 'Graham')
 # ›{'John', 'Michael', 'Terry', 'Eric', 'Graham'}
+
+
+
+
+#Sets - blazingly fast unordered Lists 
+friends = ['John','Michael','Terry','Eric','Graham']
+friends_tuple = ('John','Michael','Terry','Eric','Graham')
+friends_set = {'John','Michael','Terry','Eric','Graham','Eric'}
+my_friends_set = {'Reg','Loretta','Colin','Eric','Graham'}
+
+print(friends_set.intersection(my_friends_set))
+
+#{'Eric', 'Graham'}
+
+
