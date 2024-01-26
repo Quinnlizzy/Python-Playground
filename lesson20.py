a10465f4d115311dc590e311a6cb1ea80aa59906
Quinnlizzy <quin27@hotmail.co.uk>
2024-01-26 15:15:38 +0000

    sets union

--- lesson20.py
+++ lesson20.py
@@ -42,6 +42,24 @@ print(friends_set.difference(my_friends_set))
 
 
 #Sets - blazingly fast unordered Lists 
+#empty Lists
+empty_list = []
+empyt_list = list()
+
+#empty Tuple
+empty_tuple = ()
+empty_tuple = tuple()
+
+#empty Set
+empty_set = {} # this is wrong, this is a dictionary
+empty_set = set()
+
+#{'John', 'Michael', 'Terry', 'Eric', 'Graham', 'Reg', 'Loretta', 'Colin'}
+
+
+
+
+#Sets - blazingly fast unordered Lists 
 friends = ['John','Michael','Terry','Eric','Graham']
 friends_tuple = ('John','Michael','Terry','Eric','Graham')
 friends_set = {'John','Michael','Terry','Eric','Graham','Eric'}
