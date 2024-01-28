0a52dc59a56127d4c35fe374e64b158fb21d7cdf
Quinnlizzy <quin27@hotmail.co.uk>
2024-01-28 17:55:10 +0000

    updates

--- lesson23.py
+++ lesson23.py
@@ -30,3 +30,18 @@ greeting("Brian","32")
 
 #›Hello Brian, you are 32!
 #›Hello Brian, you are 32!
+
+
+
+
+def greeting(name,age="28"):
+    print("Hello " + name + ", you are " + age + "!")
+    print(f"Hello {name}, you are {age}!")
+    
+greeting("Brian","32")
+greeting("Judith")
+
+# ›Hello Brian, you are 32!
+# ›Hello Brian, you are 32!
+# ›Hello Judith, you are 28!
+# ›Hello Judith, you are 28!
