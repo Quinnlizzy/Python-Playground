28be49791e30fec598b2e08cb2a3d896503fa00c
Quinnlizzy <quin27@hotmail.co.uk>
2024-01-28 17:46:43 +0000

    updates

--- lesson22.py
+++ lesson22.py
@@ -8,3 +8,15 @@ print("#Comments")
 Nobody expects the 
 spanish inquisition
 '''
+
+
+print("#Comments")
+#Hiding in the comments
+# more comments
+'''
+Nobody expects the 
+spanish inquisition
+'''
+
+Traceback (most recent call last): File __main__, line 4 name = input(Enter your silly name: ) ^ SyntaxError: invalid syntax
+!Error: Unknown error
